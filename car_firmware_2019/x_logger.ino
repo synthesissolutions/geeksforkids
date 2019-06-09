ea8fdf4061c72be8ea46f0955e885d507e7c9169
David Sherrick <dsherrick@synthesissolutions.com>
2019-06-09 15:16:18 -0500

    temporary code to test the TFT display and a push button

--- car_firmware_2019/x_logger.ino
+++ car_firmware_2019/x_logger.ino
@@ -26,6 +26,9 @@ class Logger {
     RemoteControl *rc;
     Steering *steering;
     Throttle *throttle;
+    TFTDisplay *tftDisplay;
+    int pushCount = 0;
+    Button testButton;
 
   public: 
     // Default constructor ... does nothing.  This allows us to delay setting the pins until we want to (via the init method).
@@ -35,9 +38,11 @@ class Logger {
     /*
      * Set up the logging.  An updateTime of 0 will turn logging off.  Very much tied to knowing what objects we're going to log status for!!!
      */
-    void init(int updateTime, DIPSwitches *d, Configuration *c, Joystick *j, SteeringPotGoButton *pg, RemoteControl *r, Steering *s, Throttle *t) {
+    void init(int updateTime, DIPSwitches *d, Configuration *c, Joystick *j, SteeringPotGoButton *pg, RemoteControl *r, Steering *s, Throttle *t, TFTDisplay *tft) {
 
       Serial.begin(txSpeed);
+
+      testButton.init(PIN_CONTROL_NEXT);
   
       dips = d;
       configuration = c;
@@ -46,6 +51,7 @@ class Logger {
       rc = r;
       steering = s;
       throttle = t;
+      tftDisplay = tft;
       
       updateDeltaT = updateTime;
       lastUpdateTime = millis();
@@ -72,22 +78,52 @@ class Logger {
       // is logging turned on?
       if (updateDeltaT==0) return;
 
+      testButton.updateButton();
+      if (testButton.clicked()) {
+        pushCount++;
+        testButton.clearClicked();
+      }
+        
       // is it time yet to do another log (we don't want to log too much)
       if (millis()-lastUpdateTime >= updateDeltaT) {
 
+        // Clear TFT
+        tftDisplay->clearScreenBlack();
+        tftDisplay->setCursor(0,0);
+        
         // Yep ... it's time.
         Serial.print("Log Time:");Serial.println(millis());
 
+
+        Serial.print("Push Count: ");Serial.print(pushCount);
+        if (testButton.isPressed()) {
+          Serial.println(" Button Pressed!");
+        } else {
+          Serial.println(" Button NOT Pressed");
+        }
+        
+        tftDisplay->print(2, ILI9341_GREEN, "Log Time: ");
+        tftDisplay->println(2, ILI9341_GREEN, millis());
+        tftDisplay->println(2, ILI9341_DARKCYAN, dips->getMinimalStatus());
+        tftDisplay->println(2, ILI9341_DARKCYAN, configuration->getMinimalStatus());
+        tftDisplay->println(2, ILI9341_WHITE, joystick->getMinimalStatus());
+        if (configuration->useSteeringPotentiometerAndGoButton()) tftDisplay->println(2, ILI9341_WHITE, potGo->getMinimalStatus());
+        tftDisplay->println(2, ILI9341_WHITE, rc->getMinimalStatus());
+        tftDisplay->println(2, ILI9341_RED, steering->getMinimalStatus());
+        tftDisplay->println(2, ILI9341_RED, throttle->getMinimalStatus());
+
         // Go through the known instances (they'd better all be here).  Would be better to have an interface and a 
-        //   vector of these to iterate over. 
-        Serial.print(dips->getStatus());Serial.println();
+        //   vector of these to iterate over.
+        Serial.println(dips->getStatus());
         Serial.print(configuration->getStatus());Serial.println();
         Serial.print(joystick->getStatus());Serial.println();
         Serial.print(potGo->getStatus());Serial.println();
         Serial.print(rc->getStatus());Serial.println();
         Serial.print(steering->getStatus());Serial.println();
         Serial.print(throttle->getStatus());Serial.println();
-
+        Serial.print(tftDisplay->getStatus());Serial.println();
+        Serial.print(testButton.getStatus());Serial.println();
+        
         // now the ad-hoc lines... this also helps space everythig out so the log is a bit easier to read
         for (int i=0; i<20; i++) {
           Serial.println(lines[i]);   
@@ -103,9 +139,6 @@ class Logger {
 
         // and set up for the next time writeLog is called
         lastUpdateTime = millis();
-
       }
     }
-
-  
 };
