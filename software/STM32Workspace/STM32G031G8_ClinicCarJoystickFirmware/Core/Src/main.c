8724668711eab97d376c4c76176d6ac0ce8a2a24
David Sherrick <dsherrick@synthesissolutions.com>
2022-05-28 19:11:44 -0500

    adding clinic car joystick firmware

---
+++ software/STM32Workspace/STM32G031G8_ClinicCarJoystickFirmware/Core/Src/main.c
@@ -0,0 +1,329 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ TIM_HandleTypeDef htim3;
+
+/* USER CODE BEGIN PV */
+#define THROTTLE_OFF		500
+#define THROTTLE_FORWARD 	800
+#define THROTTLE_REVERSE 	200
+
+#define STEERING_STRAIGHT	500
+#define STEERING_LEFT		200
+#define STEERING_RIGHT		800
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM3_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+  uint16_t pwmSteering = STEERING_STRAIGHT;
+  uint16_t pwmThrottle = THROTTLE_OFF;
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM3_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  // All Buttons are connected to GND so their logic is inverted
+
+	  if (!HAL_GPIO_ReadPin(FORWARD_PAD_GPIO_Port, FORWARD_PAD_Pin)
+			  && !HAL_GPIO_ReadPin(LEFT_PAD_GPIO_Port, LEFT_PAD_Pin)) {
+		  // Joystick pushed forward and left
+		  pwmThrottle = THROTTLE_FORWARD;
+		  pwmSteering = STEERING_LEFT;
+	  } else if (!HAL_GPIO_ReadPin(FORWARD_PAD_GPIO_Port, FORWARD_PAD_Pin)
+			  && !HAL_GPIO_ReadPin(RIGHT_PAD_GPIO_Port, RIGHT_PAD_Pin)) {
+		  // Joystick pushed forward and right
+		  pwmThrottle = THROTTLE_FORWARD;
+		  pwmSteering = STEERING_RIGHT;
+	  } else if (!HAL_GPIO_ReadPin(FORWARD_PAD_GPIO_Port, FORWARD_PAD_Pin)) {
+		  // Joystick pushed forward
+		  pwmThrottle = THROTTLE_FORWARD;
+		  pwmSteering = STEERING_STRAIGHT;
+	  } else if (!HAL_GPIO_ReadPin(REVERSE_PAD_GPIO_Port, REVERSE_PAD_Pin)
+			  && !HAL_GPIO_ReadPin(LEFT_PAD_GPIO_Port, LEFT_PAD_Pin)) {
+		  // Joystick pushed backward and left
+		  pwmThrottle = THROTTLE_REVERSE;
+		  pwmSteering = STEERING_LEFT;
+	  } else if (!HAL_GPIO_ReadPin(REVERSE_PAD_GPIO_Port, REVERSE_PAD_Pin)
+			  && !HAL_GPIO_ReadPin(RIGHT_PAD_GPIO_Port, RIGHT_PAD_Pin)) {
+		  // Joystick pushed backward and right
+		  pwmThrottle = THROTTLE_REVERSE;
+		  pwmSteering = STEERING_RIGHT;
+	  } else if (!HAL_GPIO_ReadPin(RIGHT_PAD_GPIO_Port, RIGHT_PAD_Pin)) {
+		  // Joystick pushed right
+		  pwmThrottle = THROTTLE_OFF;
+		  pwmSteering = STEERING_RIGHT;
+	  } else if (!HAL_GPIO_ReadPin(LEFT_PAD_GPIO_Port, LEFT_PAD_Pin)) {
+		  // Joystick pushed left
+		  pwmThrottle = THROTTLE_OFF;
+		  pwmSteering = STEERING_LEFT;
+	  } else if (!HAL_GPIO_ReadPin(REVERSE_PAD_GPIO_Port, REVERSE_PAD_Pin)) {
+		  // Joystick pushed backward
+		  pwmThrottle = THROTTLE_REVERSE;
+		  pwmSteering = STEERING_STRAIGHT;
+	  } else {
+		  pwmThrottle = THROTTLE_OFF;
+		  pwmSteering = STEERING_STRAIGHT;
+	  }
+
+	  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, pwmSteering);
+	  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, pwmThrottle);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 15;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 1000;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 500;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.Pulse = 0;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_LEFT_GPIO_Port, LED_LEFT_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LED_LEFT_Pin */
+  GPIO_InitStruct.Pin = LED_LEFT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_LEFT_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : RIGHT_PAD_Pin */
+  GPIO_InitStruct.Pin = RIGHT_PAD_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(RIGHT_PAD_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LEFT_PAD_Pin */
+  GPIO_InitStruct.Pin = LEFT_PAD_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(LEFT_PAD_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : REVERSE_PAD_Pin FORWARD_PAD_Pin */
+  GPIO_InitStruct.Pin = REVERSE_PAD_Pin|FORWARD_PAD_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
